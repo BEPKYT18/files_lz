b6dd63e759ae826ca443eb1ad8bc5a96ae538825
BEPKYT18 <generaldunhill@tutamail.com>
2025-11-11 22:38:32 +0300

    7

--- lion.py
+++ lion.py
@@ -4,19 +4,18 @@ import docx
 import pandas as pd
 from collections import Counter
 import matplotlib.pyplot as plt
-k=0
+patt='[0-9,a-z]'
 doc=docx.Document('lion.docx')
 fullText = []
 
 for para in doc.paragraphs:
     fullText.append(para.text)
  
-fullText=' '.join(e for e in fullText if e.isalnum()).lower()
-Text = re.findall('[a-zа-яё]+', fullText, flags=re.IGNORECASE)
+fullText=' '.join(fullText).lower()
+fullText=re.sub(patt, '', fullText)
+Text =re.findall('[a-za-яё]+', fullText, flags=re.IGNORECASE)
 
 war={}
-
-
 
 for i in range(len(Text)):
     if Text[i] in war:
@@ -41,22 +40,18 @@ data = {
 
 print(tabulate.tabulate(data, headers='data.keys', tablefmt='fancy_grid'))
 
-patt='[0-9,a-z]'
-
-
 letters=''.join(e for e in fullText if e.isalnum())
 
-letters= re.sub(patt, '', letters)
-letters=list(letters)
-print(letters)
+#letters=list(letters)
+#print(letters)
 
 stat={}
 
-for i in range(len(letters)):
-    if letters[i] in war:
-        stat[letters[i]]+=1
-    else:
-        stat[letters[i]]=1 
+for i in letters:
+    if i not in war:
+        stat[i]=1
+    elif i in war:
+        stat[i]+=1 
 
 #plt.hist(stat.keys, color = 'blue', edgecolor = 'black',
-         #bin=int(180/5))
+        # bin=int(180/5))
