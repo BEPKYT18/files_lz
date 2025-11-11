a8e8aefabf0587682a5ca2f46e5ee02c1bccdafa
BEPKYT18 <generaldunhill@tutamail.com>
2025-11-11 19:25:08 +0300

    3

--- lion.py
+++ lion.py
@@ -1,25 +1,24 @@
+import re
 import docx
 import pandas as pd
 from collections import Counter
 k=0
 doc=docx.Document('lion.docx')
 fullText = []
+
 for para in doc.paragraphs:
     fullText.append(para.text)
-
+ 
 fullText=' '.join(fullText).lower()
-
-words = fullText.split (' ') 
+fullText = re.findall('[a-zа-яё]+', fullText, flags=re.IGNORECASE)
 
 war={}
 
-for i in words:
-    if i in war:
-        war[i]+=1
+for i in range(len(fullText)):
+    if fullText[i] in war:
+        war[fullText[i]]+=1
     else:
-        war[i]=1
-    k+=1
-
+        war[fullText[i]]=1
 
 df=pd.DataFrame(list(war.items()), columns=['Слово', 'Частота встречи, раз'])
 
