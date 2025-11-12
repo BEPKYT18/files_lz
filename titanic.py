c6cf2c31bf66a521bdf87dac74b99a9fc92a5069
BEPKYT18 <generaldunhill@tutamail.com>
2025-11-12 22:35:35 +0300

    11233434df

--- titanic.py
+++ titanic.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 data = pd.read_parquet("titanic.parquet", engine="pyarrow")
-
+t=0
 data.to_csv("titanic.csv", index=False)
 
 result={}
@@ -19,6 +19,25 @@ with open('titanic.csv', 'r') as file:
                 result[row[2]][row[1]]=1
             elif row[1] in result[row[2]]:
                 result[row[2]][row[1]]+=1
-  
+        t+=1
+        
+tclass=list(result.keys())
 
-print(result)
+live={}
+die={}
+stat=[]
+
+for i in range(len(result)):
+    live[tclass[i]]=result[tclass[i]]['1']/t*100
+    die[tclass[i]]=result[tclass[i]]['0']/t*100
+    
+
+   
+
+plt.bar(range(len(live)), list(live.values()),color='blue', align='center')
+plt.xticks(range(len(live)), list(live.keys()))
+plt.show()
+plt.bar(range(len(live)), list(live.values()),color='orange', align='center')
+plt.xticks(range(len(live)), list(live.keys()))
+plt.show()
+
